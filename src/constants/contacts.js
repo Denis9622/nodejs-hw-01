89f521cad2c98d9a577591c77d7f61f21567baf8
Denis9622 <152106321+Denis9622@users.noreply.github.com>
2024-09-08 13:15:46 +0300

    final

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,12 @@
-export const PATH_DB =
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Получаем текущий путь (__dirname не доступен в ES Modules)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Формируем путь к db.json
+const PATH_DB = path.join(__dirname, '../db/db.json');
+
+// Экспортируем путь
+export { PATH_DB };
